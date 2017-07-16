116e07f3bc7675f3d2e2e47cd6e94c1dffcc793b
Akhtar <yaseen.akhtar@sage016948.home>
2017-07-16 17:43:30 +0100

    Moved converter code

--- account.rb
+++ account.rb
@@ -2,22 +2,17 @@ require_relative 'digit'
 
 class Account
 
-  attr_reader :digits
+  attr_reader :digits, :account_number
   
   def initialize(digits)
     @digits = digits
+    @account_number = digits_to_account_number
   end
   
-
-  def to_account_number
-    @digits.inject('') { | result, d | result << d.to_account_string }
-  end
-
   def checksum
-    str = to_account_number
     checksum_total = 0
     
-    str.reverse.each_char.with_index(1) do |digit, index|
+    @account_number.reverse.each_char.with_index(1) do |digit, index|
       checksum_total += digit.to_i * index  
     end
     
@@ -25,9 +20,8 @@ class Account
   end
   
   def status
-    account_string = to_account_number
     
-    return  if account_string.include? '?'
+    return  if account_number.include? '?'
               "ILL"
             else if checksum != 0
               "ERR"
@@ -35,5 +29,11 @@ class Account
               ""
             end
   end
+
+  private
   
+  def digits_to_account_number
+    @digits.inject('') { | result, d | result << d.to_account_string }
+  end
+
 end
--- account_converter.rb
+++ account_converter.rb
@@ -12,19 +12,20 @@ class AccountConverter
       digits_in_line = split_account_line_into_digits(account_line)
 
       digits_in_line.each_with_index do | digit_line, digit_position |
-        add_digit_line(digit_line, digit_position)
+        d = get_digit_for_position(digit_position)
+        d.add_line(digit_line)
       end
     end
     
     Account.new(@digits)
   end
+  
+  private
 
   def add_digit_line(digit_segment, digit_position)
     digit = get_digit_for_position(digit_position)
     digit.add_line(digit_segment)
   end
-  
-  private
   
   def split_account_line_into_digits(account_line)
     account_line.scan(/.{3}/)
