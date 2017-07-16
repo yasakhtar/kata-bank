3876b048c9275307a357d1306efd10ae9a0a03c5
Akhtar <yaseen.akhtar@sage016948.home>
2017-07-16 17:36:31 +0100

    show account number status

--- account.rb
+++ account.rb
@@ -4,14 +4,10 @@ class Account
 
   attr_reader :digits
   
-  def initialize
-    @digits = []
+  def initialize(digits)
+    @digits = digits
   end
-
-  def add_digit_line(digit_segment, digit_position)
-    digit = get_digit_for_position(digit_position)
-    digit.add_line(digit_segment)
-  end
+  
 
   def to_account_number
     @digits.inject('') { | result, d | result << d.to_account_string }
@@ -39,17 +35,5 @@ class Account
               ""
             end
   end
-
-  private
-
-  def get_digit_for_position(pos)
-    ensure_digit_exists_at_pos(pos)
-    @digits[pos]
-
-  end
-
-  def ensure_digit_exists_at_pos(pos)
-    @digits << Digit.new if @digits.length <= pos
-  end
-
+  
 end
--- account_converter.rb
+++ account_converter.rb
@@ -5,18 +5,23 @@ class AccountConverter
   # handle the 4 lines for an account - returns the list of digits in the account.
   def to_account(account_lines)
     
-    account = Account.new
+    @digits = []
     
     account_lines.each do | account_line |
       
       digits_in_line = split_account_line_into_digits(account_line)
 
       digits_in_line.each_with_index do | digit_line, digit_position |
-        account.add_digit_line(digit_line, digit_position)
+        add_digit_line(digit_line, digit_position)
       end
     end
     
-    account
+    Account.new(@digits)
+  end
+
+  def add_digit_line(digit_segment, digit_position)
+    digit = get_digit_for_position(digit_position)
+    digit.add_line(digit_segment)
   end
   
   private
@@ -24,5 +29,14 @@ class AccountConverter
   def split_account_line_into_digits(account_line)
     account_line.scan(/.{3}/)
   end
-  
+
+  def get_digit_for_position(pos)
+    ensure_digit_exists_at_pos(pos)
+    @digits[pos]
+  end
+
+  def ensure_digit_exists_at_pos(pos)
+    @digits << Digit.new if @digits.length <= pos
+  end
+
 end
